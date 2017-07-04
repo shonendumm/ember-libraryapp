ce87ac4c8ebeb27c60fd3d14faa5db9b321e24e4
SooHian <shonendumm@gmail.com>
2017-07-04 17:14:32 +0800

    Add admin page to see all invitations

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -6,15 +6,17 @@ export default Ember.Controller.extend({
     isEmailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
 
     message: '',
-    isMessageValid: Ember.computed.gte('message', 5),
+    isMessageValid: Ember.computed.gte('message.length', 5),
+
 
 // whether to disable the button based on validations above:
-    isDisabled: Ember.computed.not('isEmailValid', 'isMessageValid'),
+    isValid: Ember.computed.and('isEmailValid', 'isMessageValid'),
+    isDisabled: Ember.computed.not('isValid'),
 
     actions: {
 
       contactUs() {
-        
+
         this.set('responseMessage', `Thank you! Your message has been sent.`);
         this.set('emailAddress', '');
         this.set('message', '');
