79e7b76ef32e84ee4953901e60185c4f02f6e4ee
SooHian <shonendumm@gmail.com>
2017-07-04 14:40:15 +0800

    Request invitation button enabled

---
+++ app/controllers/index.js
@@ -0,0 +1,11 @@
+// app/controllers/index.js
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid')
+
+});
